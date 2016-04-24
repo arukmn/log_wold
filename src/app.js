3b7e1fa3f6383adb10d4c424ddb65fd341bcf8bb
arukmn <t.kuriyama@air-closet.com>
2016-04-25 07:36:10 +0900

    initial commit

---
+++ src/app.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+
+// Reducer has the interface `(state, action) => state`
+const text = (state = { text: '' }, action) => {
+  switch (action.type) {
+    case 'UPDATE_TEXT':
+      return { text: action.text };
+    default:
+      return state;
+  }
+};
+
+const mapStateToProps = (state) => {
+  return {
+    text: state.text,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onChange: (e) => dispatch({
+      type: 'UPDATE_TEXT',
+      text: e.target.value,
+    }),
+  };
+};
+
+let WordCountBox = ({ txt, onChange }) => {
+  return (
+    <div className="wordCountBox">
+      <h1>Hello, Redux!</h1>
+      <textarea
+        rows="8"
+        cols="80"
+        placeholder="Type something..."
+        autoFocus="true"
+        onChange={onChange}
+      >
+        {txt}
+      </textarea>
+      <p>Count: {text.length}</p>
+    </div>
+  );
+};
+WordCountBox = connect(mapStateToProps, mapDispatchToProps)(WordCountBox);
+
+ReactDOM.render(
+  <Provider store={createStore(text)}>
+    <WordCountBox />
+  </Provider>,
+  document.getElementById('content')
+);
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: __dirname + '/lib',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            'react',
+            'es2015'
+          ]
+        }
+      }
+    ]
+  }
+};
