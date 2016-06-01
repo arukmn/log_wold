761e9851d85aae6a1c6757423b0c7d05912e7430
arukmn <t.kuriyama@air-closet.com>
2016-06-02 03:22:52 +0900

    add appbar material theme

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import { cyan500 } from 'material-ui/styles/colors'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import AppBar from 'material-ui/AppBar'
 import VisibleTodoList from '../containers/VisibleTodoList'
 import Footer from '../components/Footer'
 import todo from '../reducers/'
 import AddTodo from '../containers/AddTodo'
 import { addTodo, toggleTodo, setVisibilityFilter } from '../actions/'
+
+const muiTheme = getMuiTheme({
+  palette: {
+    textColor: cyan500,
+  },
+  appBar: {
+    height: 80,
+  },
+})
 
 // reducer: todoApp
 let store = createStore(todo)
@@ -19,13 +32,16 @@ store.dispatch(toggleTodo(0))
 store.dispatch(setVisibilityFilter('SHOW_COMPLETED'))
 
 const App = () => (
-  <Provider store={store}>
-    <div>
-      <AddTodo />
-      <VisibleTodoList />
-      <Footer />
-    </div>
-  </Provider>
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <Provider store={store}>
+      <div>
+        <AppBar title="Logger" />
+        <AddTodo />
+        <VisibleTodoList />
+        <Footer />
+      </div>
+    </Provider>
+  </MuiThemeProvider>
 )
 
 export default App
